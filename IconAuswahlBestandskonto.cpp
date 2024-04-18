c69f73d352571f1db31eed9c59f5e82d4769580a
Darkwing371 <darkwing371@gmx.de>
2024-04-18 18:47:58 +0200

    Neue Icons für Bestandkonten (Bitcoin, FinTechs).

--- IconAuswahlBestandskonto.cpp
+++ IconAuswahlBestandskonto.cpp
@@ -3,20 +3,20 @@
 // Diese Datei ist Bestandteil von EasyCash&Tax, der freien EÜR-Fibu
 //
 // Copyleft (GPLv3) 2020  Thomas Mielke
-// 
-// Dies ist freie Software; Sie dürfen sie unter den Bedingungen der 
-// GNU General Public License, wie von der Free Software Foundation 
-// veröffentlicht, weiterverteilen und/oder modifizieren; entweder gemäß 
+//
+// Dies ist freie Software; Sie dürfen sie unter den Bedingungen der
+// GNU General Public License, wie von der Free Software Foundation
+// veröffentlicht, weiterverteilen und/oder modifizieren; entweder gemäß
 // Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren Version.
 //
-// Diese Software wird in der Hoffnung weiterverbreitet, dass sie nützlich 
-// sein wird, jedoch OHNE IRGENDEINE GARANTIE, auch ohne die implizierte 
+// Diese Software wird in der Hoffnung weiterverbreitet, dass sie nützlich
+// sein wird, jedoch OHNE IRGENDEINE GARANTIE, auch ohne die implizierte
 // Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK.
 // Mehr Details finden Sie in der GNU Lesser General Public License.
 //
-// Sie sollten eine Kopie der GNU General Public License Version 3 zusammen mit 
-// dieser Software erhalten haben; falls nicht, schreiben Sie an die Free 
-// Software Foundation, Inc., 51 Franklin St, 5th Floor, Boston, MA 02110, USA. 
+// Sie sollten eine Kopie der GNU General Public License Version 3 zusammen mit
+// dieser Software erhalten haben; falls nicht, schreiben Sie an die Free
+// Software Foundation, Inc., 51 Franklin St, 5th Floor, Boston, MA 02110, USA.
 
 #include "stdafx.h"
 #include "easycash.h"
@@ -182,7 +182,17 @@ static IconInfo Icons[] = {
 	"ICICI Bank",
 	"IKBdirekt",
 	"MLP",
-	"Amerika Express Card"
+	"American Express",
+	// 6 bitcoinbezogene Icons
+	"Bitcoin",
+	"Lightning",
+	"BTCPay Server",
+	"Bitcart",
+	"Lipa",
+	"Opago",
+	// FinTechs
+	"Wise",
+	"Revolut"
 };
 
 IconInfo* CIconAuswahlBestandskonto::GetIcons()
@@ -209,26 +219,26 @@ BOOL CIconAuswahlBestandskonto::ChooseProperty(CString &csProperty)
 		csProperty = dlg.m_csAnfangssaldo;
 		return TRUE;
 	}
-	else 
+	else
 		return FALSE;
 
 }
 
 void CIconAuswahlBestandskonto::Umbenennen(int nElement)
 {
-	
+
 }
 
 void CIconAuswahlBestandskonto::Loeschen(int nElement)
 {
-	
+
 }
 
 int CIconAuswahlBestandskonto::IconAuswahl()
 {
 	// Icon wählen
 	CIconAuswahlBestandskonto dlgIcon(m_pParent);
-	dlgIcon.DoModal();		
+	dlgIcon.DoModal();
 
 	return dlgIcon.m_nSelected;
 }
